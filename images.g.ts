7f1f7a3b4590798265d8d1854389091d41daa61e
Axelclouet <axelclouet4064@gmail.com>
2025-02-13 14:39:15 +0100

    Mise à jour pxt.json, images.g.jres, images.g.ts

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . f f 2 2 f f . . . . . 
+. . . . f 2 2 2 2 2 2 f . . . . 
+. . . f 2 2 2 2 2 2 2 2 f . 2 2 
+. . f 2 2 2 2 2 2 2 2 2 2 2 2 . 
+. . f 2 2 2 2 2 2 2 2 2 2 2 2 2 
+. f 3 3 3 3 3 3 3 3 3 3 3 3 f . 
+. f 3 3 f f e e e e f f 3 3 f . 
+. f b b f b f e e f b f b b f . 
+. f b b e 1 f e e f 1 e b b f . 
+f f b b f e e e e e e f b b f f 
+f b b e 1 1 1 1 1 1 1 1 e b b f 
+f f f e 1 1 1 1 1 1 1 1 e e f f 
+. . . e 1 1 1 1 1 1 1 1 e e . . 
+. . . e 1 1 1 1 1 1 1 1 e e . . 
+. . . e 1 1 1 1 1 1 1 1 e . . . 
+. . . . . f f b b f f . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
